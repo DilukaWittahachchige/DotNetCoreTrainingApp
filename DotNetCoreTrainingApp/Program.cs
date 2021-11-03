42dd0ea5272515720adfb878a3f894aae8f8a923
Diluka Wittahachchige <diluka.99999@outlook.com>
2021-11-03 17:55:16 +0800

    Add project files.

---
+++ DotNetCoreTrainingApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNetCoreTrainingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
