6ed08311532b102978db8ab928503a7904981cfb
Diluka Wittahachchige <diluka.99999@outlook.com>
2021-11-08 16:15:16 +0800

    init app - HelloWorld

--- DotNetCoreTrainingApp/Program.cs
+++ DotNetCoreTrainingApp/Program.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿// Import components to use in the file
+using System;
 
+// A namespace is a logical grouping of components
 namespace DotNetCoreTrainingApp
 {
+    // A class is a kind of component
     class Program
     {
+        // The Main method is the entrypoint of an application
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!"); // Instructions separated by ";"
         }
     }
+
+    /* Comments
+       can also
+       span several lines */
 }
