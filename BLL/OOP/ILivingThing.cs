fe6f7d2a5f1fd050f0108b578cbd9b8f4e24c2da
Diluka Wittahachchige <diluka.99999@outlook.com>
2021-11-14 17:16:26 +0800

    OOP   - Interface

---
+++ BLL/OOP/ILivingThing.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.OOP
+{
+    //Interface
+    public interface ILivingThing
+    {
+        //method
+        bool Age();
+    }
+}
